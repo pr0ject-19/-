cfdb4cd158d077eddf1ae91c1b2a5c7a5b0d90ea
BuildTools <unconfigured@null.spigotmc.org>
2020-05-26 22:48:13 +0200

    Updates

---
+++ precache-manifest.0eeb7fe46cbd5202fae99eaacb27d326.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba533fcce22b68eb5d1658f3b9d00110",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2538192988ac7a4ed7e8",
+    "url": "/static/css/main.e3950877.chunk.css"
+  },
+  {
+    "revision": "d015dc02e3fcc98cc5d5",
+    "url": "/static/js/2.31728d4c.chunk.js"
+  },
+  {
+    "revision": "507c4436aa55e48136618e82a79cbf78",
+    "url": "/static/js/2.31728d4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2538192988ac7a4ed7e8",
+    "url": "/static/js/main.d93fe667.chunk.js"
+  },
+  {
+    "revision": "1a4e793ece9a4255a1f2",
+    "url": "/static/js/runtime-main.a19ced1f.js"
+  },
+  {
+    "revision": "0a353697d60c406f90ed777a5689d81b",
+    "url": "/static/media/left-arrow.0a353697.svg"
+  }
+]);
