388f24f2cfbf8df0e454076c674a6172c95e50dd
BuildTools <unconfigured@null.spigotmc.org>
2020-05-26 23:16:49 +0200

    Updates

---
+++ precache-manifest.25fe6b99b8e9e95539e4aa6000d94a7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c63c71faf6b606234733e91f2f49488",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "67d4b16daa3f2a11f582",
+    "url": "/covid19/static/css/main.e3950877.chunk.css"
+  },
+  {
+    "revision": "d015dc02e3fcc98cc5d5",
+    "url": "/covid19/static/js/2.31728d4c.chunk.js"
+  },
+  {
+    "revision": "507c4436aa55e48136618e82a79cbf78",
+    "url": "/covid19/static/js/2.31728d4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67d4b16daa3f2a11f582",
+    "url": "/covid19/static/js/main.61b59825.chunk.js"
+  },
+  {
+    "revision": "f3d71a6c729d8ced3380",
+    "url": "/covid19/static/js/runtime-main.6f0157b4.js"
+  },
+  {
+    "revision": "0a353697d60c406f90ed777a5689d81b",
+    "url": "/covid19/static/media/left-arrow.0a353697.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0eeb7fe46cbd5202fae99eaacb27d326.js"
+  "/covid19/precache-manifest.25fe6b99b8e9e95539e4aa6000d94a7c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid19/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
